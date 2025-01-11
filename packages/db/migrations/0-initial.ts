1567c2f8f453e37b7cf05af80749911e21e28c3e
Jason Cameron <git@jasoncameron.dev>
2025-01-10 22:00:37 -0500

    feat: Migrate database schema
    This commit refactors the database schema, replacing the `posts` table with `hackathons` and removing several unnecessary migration files.  The `hackathons` table now includes fields for `archivedAt` and `archivedByUserId`, providing more comprehensive data tracking.  Additionally,  the initial migration (0-initial.ts) has been significantly updated to reflect the new schema structure.

--- packages/db/migrations/0-initial.ts
+++ packages/db/migrations/0-initial.ts
@@ -1,18 +1,31 @@
 import { Kysely } from 'kysely'
-import { SnowflakeDataType, uuidColumnBuilder } from '../migrations-utils.js'
+import { uuidColumnBuilder } from '../migrations-utils.js'
 
 export async function up(db: Kysely<any>): Promise<void> {
-  // --- Users
+  // Create categories table
+  await db.schema
+    .createTable('categories')
+    .addColumn('id', 'uuid', uuidColumnBuilder)
+    .addColumn('snowflakeId', 'integer', (col) => col.notNull().unique())
+    .addColumn('name', 'text', (col) => col.notNull())
+    .addColumn('description', 'text', (col) => col.notNull())
+    .execute()
+
+  // Create users table
   await db.schema
     .createTable('users')
     .addColumn('id', 'uuid', uuidColumnBuilder)
-    .addColumn('snowflakeId', SnowflakeDataType, (col) =>
-      col.notNull().unique(),
-    )
-    .addColumn('isPrivate', 'boolean', (col) => col.notNull().defaultTo(false))
+    .addColumn('snowflakeId', 'integer', (col) => col.notNull().unique())
     .addColumn('username', 'text', (col) => col.notNull())
     .addColumn('discriminator', 'varchar(4)', (col) => col.notNull())
     .addColumn('avatarUrl', 'text', (col) => col.notNull())
+    .addColumn('joinedAt', 'date')
+    .addColumn('isPrivate', 'boolean', (col) => col.notNull().defaultTo(false))
+    .addColumn('answersCount', 'integer', (col) => col.notNull().defaultTo(0))
+    .addColumn('points', 'integer', (col) => col.notNull().defaultTo(0))
+    .addColumn('isModerator', 'boolean', (col) =>
+      col.notNull().defaultTo(false),
+    )
     .execute()
 
   await db.schema
@@ -21,65 +34,79 @@ export async function up(db: Kysely<any>): Promise<void> {
     .column('snowflakeId')
     .execute()
 
-  // -- Channels
+  // Create channels table
   await db.schema
     .createTable('channels')
     .addColumn('id', 'uuid', uuidColumnBuilder)
-    .addColumn('snowflakeId', SnowflakeDataType, (col) =>
-      col.notNull().unique(),
-    )
+    .addColumn('snowflakeId', 'integer', (col) => col.notNull().unique())
     .addColumn('name', 'text', (col) => col.notNull())
-    .addColumn('type', 'int2', (col) => col.notNull())
+    .addColumn('type', 'integer', (col) => col.notNull())
     .addColumn('topic', 'text', (col) => col.notNull())
+    .addColumn('categoryId', 'uuid', (col) =>
+      col.references('categories.id').onDelete('set null'),
+    )
     .execute()
 
-  // -- Posts
   await db.schema
-    .createTable('posts')
+    .createIndex('channels_categoryId_idx')
+    .on('channels')
+    .column('categoryId')
+    .execute()
+
+  // Create hackathons table
+  await db.schema
+    .createTable('hackathons')
     .addColumn('id', 'uuid', uuidColumnBuilder)
-    .addColumn('snowflakeId', SnowflakeDataType, (col) =>
-      col.notNull().unique(),
-    )
+    .addColumn('snowflakeId', 'integer', (col) => col.notNull().unique())
     .addColumn('title', 'text', (col) => col.notNull())
-    .addColumn('isLocked', 'boolean', (col) => col.notNull())
     .addColumn('createdAt', 'timestamptz', (col) => col.notNull())
     .addColumn('editedAt', 'timestamptz')
-    .addColumn('userId', SnowflakeDataType)
-    .addColumn('channelId', SnowflakeDataType)
-    .addColumn('answerId', SnowflakeDataType)
+    .addColumn('channelId', 'uuid', (col) =>
+      col.references('channels.id').onDelete('set null'),
+    )
+    .addColumn('isIndexed', 'boolean', (col) => col.notNull().defaultTo(false))
+    .addColumn('lastActiveAt', 'timestamptz', (col) => col.notNull())
+    .addColumn('archivedAt', 'timestamptz')
+    .addColumn('archivedByUserId', 'uuid', (col) =>
+      col.references('users.id').onDelete('set null'),
+    )
     .execute()
 
   await db.schema
-    .createIndex('posts_snowflakeId_idx')
-    .on('posts')
+    .createIndex('hackathons_snowflakeId_idx')
+    .on('hackathons')
     .column('snowflakeId')
     .execute()
 
   await db.schema
-    .createIndex('posts_userId_idx')
-    .on('posts')
-    .column('userId')
-    .execute()
-
-  await db.schema
-    .createIndex('posts_channelId_idx')
-    .on('posts')
+    .createIndex('hackathons_channelId_idx')
+    .on('hackathons')
     .column('channelId')
     .execute()
 
-  // -- Messages
+  await db.schema
+    .createIndex('hackathons_archivedByUserId_idx')
+    .on('hackathons')
+    .column('archivedByUserId')
+    .execute()
+
+  // Create messages table
   await db.schema
     .createTable('messages')
     .addColumn('id', 'uuid', uuidColumnBuilder)
-    .addColumn('snowflakeId', SnowflakeDataType, (col) =>
-      col.notNull().unique(),
-    )
+    .addColumn('snowflakeId', 'integer', (col) => col.notNull().unique())
     .addColumn('content', 'text', (col) => col.notNull())
     .addColumn('createdAt', 'timestamptz', (col) => col.notNull())
     .addColumn('editedAt', 'timestamptz')
-    .addColumn('userId', SnowflakeDataType, (col) => col.notNull())
-    .addColumn('postId', SnowflakeDataType, (col) => col.notNull())
-    .addColumn('replyToMessageId', SnowflakeDataType)
+    .addColumn('userId', 'uuid', (col) =>
+      col.notNull().references('users.id').onDelete('cascade'),
+    )
+    .addColumn('postId', 'uuid', (col) =>
+      col.notNull().references('hackathons.id').onDelete('cascade'),
+    )
+    .addColumn('replyToMessageId', 'uuid', (col) =>
+      col.references('messages.id').onDelete('set null'),
+    )
     .execute()
 
   await db.schema
@@ -106,17 +133,17 @@ export async function up(db: Kysely<any>): Promise<void> {
     .column('replyToMessageId')
     .execute()
 
-  // -- Attachments
+  // Create attachments table
   await db.schema
     .createTable('attachments')
     .addColumn('id', 'uuid', uuidColumnBuilder)
-    .addColumn('snowflakeId', SnowflakeDataType, (col) =>
-      col.notNull().unique(),
-    )
+    .addColumn('snowflakeId', 'integer', (col) => col.notNull().unique())
     .addColumn('url', 'text', (col) => col.notNull())
     .addColumn('name', 'text', (col) => col.notNull())
     .addColumn('contentType', 'text')
-    .addColumn('messageId', SnowflakeDataType, (col) => col.notNull())
+    .addColumn('messageId', 'uuid', (col) =>
+      col.notNull().references('messages.id').onDelete('cascade'),
+    )
     .execute()
 
   await db.schema
@@ -133,9 +160,11 @@ export async function up(db: Kysely<any>): Promise<void> {
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
+  // Drop tables in reverse order to avoid constraint errors
   await db.schema.dropTable('attachments').execute()
   await db.schema.dropTable('messages').execute()
-  await db.schema.dropTable('posts').execute()
+  await db.schema.dropTable('hackathons').execute()
   await db.schema.dropTable('channels').execute()
+  await db.schema.dropTable('categories').execute()
   await db.schema.dropTable('users').execute()
 }
